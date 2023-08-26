0eb61b79a9abd2498ef66829531b573de027c557
Savelyyy <129987056+Savelyyy@users.noreply.github.com>
2023-08-26 13:28:51 +0300

    Add files via upload

---
+++ Task_3.2/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input = "racecar"; //Введите своё слово.
+        if (isPalindrome(input)) {
+            System.out.println("Строка является палиндромом.");
+        } else {
+            System.out.println("Строка не является палиндромом.");
+        }
+    }
+}
